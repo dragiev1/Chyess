f304e3e49b067d3817ca1466504a4b7890a869ab
dragiev1 <dragievs2020@gmail.com>
2026-03-22 14:46:31 -0400

    fix: updated homepage url to /Chyess

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ import Register from "./pages/Register";
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/Chyess" element={<HomePage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Routes>
