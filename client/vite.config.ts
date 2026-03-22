5b56ee373f7b3253793780a1b4d0059a57c45212
dragiev1 <dragievs2020@gmail.com>
2026-03-22 14:42:19 -0400

    feat: deploying to github pages for static viewing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+import { fileURLToPath } from "url";  // ← Add this
+
+// Fix __dirname for ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Chyess/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
